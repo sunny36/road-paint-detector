7bd8255b927fc6fcc9e096315d4546f573a602a2
Somchok Sakjiraphong <s@sunny.in.th>
2010-02-25 05:04:25 +0700

    fix bug in camera matrix and wrote also wrote tests for it

--- camera.cpp
+++ camera.cpp
@@ -1,10 +1,20 @@
 #include "camera.h"
 
-Camera::Camera(){
+Camera::Camera(float focal_length, int width, int height){
+  const float THETA = 90; //degrees 
+  const float X_CW = 0.0; //meters 
+  const float Y_CW = 2.15; //meters
+  const float Z_CW = 1.00; //meters
+
   K = cvCreateMat(3, 3, CV_32FC1);
-  R = cvCreateMat(3, 3, CV_32FC1);
-  T = cvCreateMat(3, 1, CV_32FC1);
+  Rcw = cvCreateMat(3, 3, CV_32FC1);
+  Tcw = cvCreateMat(3, 1, CV_32FC1);
   P = cvCreateMat(3, 4, CV_32FC1);
+
+  //construct Rcw and Tcw
+  this->setRcw(THETA);
+  this->setTcw(X_CW, Y_CW, Z_CW);
+  this->setK(focal_length, width, height);
 }
 
 void Camera::setK(float focal_length, int width, int height){
@@ -27,7 +37,7 @@ CvMat* Camera::getK(){
 	return K;
 }
 
-void Camera::setR(float theta){
+void Camera::setRcw(float theta){
 /*
 Rx = 1        0               0
      0        cos(theta)      sin(theta)
@@ -35,24 +45,24 @@ Rx = 1        0               0
 */	
 	
 	theta = degreesToRadians(theta); // theta in radians
-	cvmSet(this->R, 0, 0, 1);
-	cvmSet(this->R, 0, 1, 0);
-	cvmSet(this->R, 0, 2, 0);
-	cvmSet(this->R, 1, 0, 0);
-	cvmSet(this->R, 1, 1, cos(theta));
-	cvmSet(this->R, 1, 2, sin(theta));
-	cvmSet(this->R, 2, 0, 0);
-	cvmSet(this->R, 2, 1, -sin(theta));
-	cvmSet(this->R, 2, 2, cos(theta));
+	cvmSet(this->Rcw, 0, 0, 1);
+	cvmSet(this->Rcw, 0, 1, 0);
+	cvmSet(this->Rcw, 0, 2, 0);
+	cvmSet(this->Rcw, 1, 0, 0);
+	cvmSet(this->Rcw, 1, 1, 0);
+	cvmSet(this->Rcw, 1, 2, 1);
+	cvmSet(this->Rcw, 2, 0, 0);
+	cvmSet(this->Rcw, 2, 1, -1);
+	cvmSet(this->Rcw, 2, 2, 0);
 	
   return;
 }
 
-CvMat* Camera::getR(){
-  return R;
+CvMat* Camera::getRcw(){
+  return Rcw;
 }
 
-void Camera::setT(float x, float y, float z){
+void Camera::setTcw(float x, float y, float z){
 /*
 World Co-ordinate system 
 	Z
@@ -66,39 +76,61 @@ World Co-ordinate system
 	    \ X
 */
 
-  cvmSet(this->T,0,0, x);
-	cvmSet(this->T,1,0, y);
-	cvmSet(this->T,2,0, z);	
+  cvmSet(this->Tcw,0,0, x);
+	cvmSet(this->Tcw,1,0, y);
+	cvmSet(this->Tcw,2,0, z);	
   
   return;
 }
 
-CvMat* Camera::getT(){	
-  return T;
+CvMat* Camera::getTcw(){	
+  return Tcw;
 }
 
 CvMat* Camera::getP(){
   CvMat *K, *R, *T; 
   K = this->getK();
-  R = this->getR(); 
-  T = this->getT();
-	CvMat* Rt = cvCreateMat(3, 4, CV_32FC1); //concatenates R and T
-	CvMat* P = cvCreateMat(3, 4, CV_32FC1);
-	int row, col;
-	for(row = 0; row < R->rows; row++){
-		for(col =0; col < R->cols; col++){
-			cvmSet(Rt, row, col, cvmGet(R, row, col));
-		}		
-	}
-	
-	for(row = 0; row < T->rows; row++){
-		for(col = 0; col < T->cols; col++){
-			cvmSet(Rt, row, col + 3, cvmGet(T, row, col));
-		}
-	}
-	
-	cvMatMul(K, Rt, P);
+	CvMat* RTcw = this->getRTcw();
+
+  //IO io;
+  /*io.printMatrixFloat(RTcw, RTcw->rows, RTcw->cols);*/
+
+  CvMat* RTwc = cvCreateMat(4, 4, CV_32FC1); 
+  cvInv(RTcw, RTwc); 
+
+  CvMat RTwc_row_1_to_3;
+  cvGetRows(RTwc, &RTwc_row_1_to_3, 0, 3); 
+	cvMatMul(K, &RTwc_row_1_to_3, P);
+  
+  //io.printMatrixFloat(P, P->rows, P->cols); 
+
 	return P;
+}
+
+
+CvMat* Camera::getRTcw(){
+  Rcw = this->getRcw(); 
+  Tcw = this->getTcw();
+	CvMat* RTcw = cvCreateMat(4, 4, CV_32FC1);
+	
+	//copy Rcw to RTcw
+  int row, col; 
+  for(row = 0; row < Rcw->rows; row++){
+    for(col = 0; col < Rcw->cols; col++){
+      cvmSet(RTcw, row, col, cvmGet(Rcw, row, col));
+    }
+  }
+  //copy Tcw to RTcw
+  for(row = 0; row < Tcw->rows; row++){
+    cvmSet(RTcw, row, 3, cvmGet(Tcw, row, 0));
+  }
+  //last row of RTcw = [0 0 0 1]
+  cvmSet(RTcw, 3, 0, 0);
+  cvmSet(RTcw, 3, 1, 0);
+  cvmSet(RTcw, 3, 2, 0);
+  cvmSet(RTcw, 3, 3, 1);
+  
+	return RTcw;
 }
 
 float Camera::degreesToRadians(float degrees){
@@ -119,51 +151,51 @@ float Camera::calculateY(int current_row){
 }
 
 
-CvPoint2D32f Camera::imageToGroundPlane(CvPoint pt){
-/*
-       | p_11 p_12 p_14 |
-  H =  | p_21 p_22 p_24 |
-       | p_31 p_32 p_34 |
-       
-*/
-  CvMat *H = cvCreateMat( 3, 3, CV_32FC1); 
-  //copy col 1 and 2 from K to H
-  int row, col; 
-  CvMat* P = getP();
-  for(row = 0; row < P->rows ; row++){
-    for(col = 0; col < 2; col++){
-      cvmSet(H, row, col, cvmGet(P, row, col));
-    }
-  }
-  //copy col 4 from K to H
-  for(row = 0; row < P->rows; row++){
-    cvmSet(H, row, 2, cvmGet(P, row, 3));
-  }
-#if defined(DEBUG)
-  IO io;
-  io.printMatrixFloat(P, P->rows, P->cols);
-  printf("\n");
-  io.printMatrixFloat(H, H->rows, H->cols);
-#endif
-
-  CvMat *H_inv = cvCreateMat( 3, 3, CV_32FC1); 
-  cvInv(H, H_inv); 
-
-  CvMat *image_point = cvCreateMat(3, 1, CV_32FC1);
-  cvmSet(image_point, 0, 0, pt.x);
-  cvmSet(image_point, 1, 0, pt.y);
-  cvmSet(image_point, 2, 0, 1);
-
-  CvMat *ground_point = cvCreateMat(3, 1, CV_32FC1);
-  cvMatMul(H_inv, image_point, ground_point);
-
-  /*IO io;*/
-  /*io.printMatrixFloat(P, P->rows, P->cols);*/
-  /*printf("\n");*/
-  /*io.printMatrixFloat(H, H->rows, H->cols);*/
-  /*printf("\n");*/
-  /*io.printMatrixFloat(ground_point, ground_point->rows, ground_point->cols);*/
-  /*printf("\n");*/
- 
-  return cvPoint2D32f(cvmGet(ground_point, 0, 0), cvmGet(ground_point, 1, 0));
-}
+// CvPoint2D32f Camera::imageToGroundPlane(CvPoint pt){
+// // 
+// //        | p_11 p_12 p_14 |
+// //   	H = | p_21 p_22 p_24 |
+// //        | p_31 p_32 p_34 |
+// // 
+// // 
+//   CvMat *H = cvCreateMat( 3, 3, CV_32FC1); 
+//   //copy col 1 and 2 from K to H
+//   int row, col; 
+//   CvMat* P = getP();
+//   for(row = 0; row < P->rows ; row++){
+//     for(col = 0; col < 2; col++){
+//       cvmSet(H, row, col, cvmGet(P, row, col));
+//     }
+//   }
+//   //copy col 4 from K to H
+//   for(row = 0; row < P->rows; row++){
+//     cvmSet(H, row, 2, cvmGet(P, row, 3));
+//   }
+// #if defined(DEBUG)
+//   IO io;
+//   io.printMatrixFloat(P, P->rows, P->cols);
+//   printf("\n");
+//   io.printMatrixFloat(H, H->rows, H->cols);
+// #endif
+// 
+//   CvMat *H_inv = cvCreateMat( 3, 3, CV_32FC1); 
+//   cvInv(H, H_inv); 
+// 
+//   CvMat *image_point = cvCreateMat(3, 1, CV_32FC1);
+//   cvmSet(image_point, 0, 0, pt.x);
+//   cvmSet(image_point, 1, 0, pt.y);
+//   cvmSet(image_point, 2, 0, 1);
+// 
+//   CvMat *ground_point = cvCreateMat(3, 1, CV_32FC1);
+//   cvMatMul(H_inv, image_point, ground_point);
+// 
+//   /*IO io;*/
+//   /*io.printMatrixFloat(P, P->rows, P->cols);*/
+//   /*printf("\n");*/
+//   /*io.printMatrixFloat(H, H->rows, H->cols);*/
+//   /*printf("\n");*/
+//   /*io.printMatrixFloat(ground_point, ground_point->rows, ground_point->cols);*/
+//   /*printf("\n");*/
+//  
+//   return cvPoint2D32f(cvmGet(ground_point, 0, 0), cvmGet(ground_point, 1, 0));
+// }
