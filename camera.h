e71399947b1d5222d1cae2ab7913a8750f4fc23e
sunny <sunny@19abc578-2ba4-4409-a2b0-026fe526f0c7>
2010-03-14 18:12:39 +0000

    fixed conflict
    git-svn-id: https://se.cs.ait.ac.th/svn/CarVideo/Code/mit-road-paint-detector/trunk@123 19abc578-2ba4-4409-a2b0-026fe526f0c7

--- camera.h
+++ camera.h
@@ -12,19 +12,15 @@ class Camera{
     CvMat* getRcw(); 
     CvMat* getTcw();
     CvMat* getP(); 
-		CvMat* getRTcw();
     float calculateY(int current_row);
-    //CvPoint2D32f imageToGroundPlane(CvPoint pt);
+    CvPoint2D32f imageToGroundPlane(CvPoint pt);
 
   private:
     CvMat *K;
     CvMat *Rcw;
     CvMat *Tcw;
     CvMat *P;
-<<<<<<< HEAD
-=======
-    CvMat* getRTcw();
->>>>>>> 31726e09faae4be7cbb17deaf97cfbad0140f9bf
+    CvMat* getRTcw();    
     float degreesToRadians(float degrees);
     void setK(float focal_lenght, int width, int height);
     void setRcw(float theta);
