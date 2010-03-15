47c37545631f88b21c961ae285b81df0e0219463
Somchok Sakjiraphong <somchok@somchok-desktop.(none)>
2010-03-15 18:48:32 +0700

    updated test code

--- tests/convolution_unittest.cpp
+++ tests/convolution_unittest.cpp
@@ -1,20 +1,25 @@
 #include <limits.h>
-#include "convolution.h"
+#include "../convolution.h"
 #include <gtest/gtest.h>
 #include <cv.h>
 #include <highgui.h>
+#include <stdlib.h> 
+#include <iostream>
+
 // Tests Factorial().
 
 // Tests factorial of negative numbers.
 TEST(kernel1D, LaneWidthIsEven) {
   
-  float expected_kernel[] = { -1, -0.5, 0, 1, 1, 1, 0, -0.5, -1 };
-  int expected_kernel_size = sizeof(expected_kernel)/sizeof(expected_kernel[0]);
-  Convolution convolution; 
-  float* actual_kernel = convolution.kernel1D(4);
+  float data[] = { -1, -0.5, 0, 1, 1, 1, 0, -0.5, -1 };
+  std::vector<float> expected_kernel(data, data + sizeof(data)/sizeof(float));
+
+  Convolution convolution;
+  std::vector<float> actual_kernel;
+  convolution.kernel1D(4, actual_kernel);
   int i; 
-  for(i = 0; i < expected_kernel_size; i++){
-    EXPECT_EQ(expected_kernel[i], actual_kernel[i]);
+  for(i = 0; i < expected_kernel.size(); i++){
+   EXPECT_EQ(expected_kernel[i], actual_kernel[i]);
   }
 }
 
